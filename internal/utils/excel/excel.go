be76ef9b5f8bdeb3b934b53dd63950a79b82e5b0
GlenSHIHSHIH <a0935745150@gmail.com>
2021-09-17 00:34:27 +0800

    修改 excel 寫入方式

--- internal/utils/excel/excel.go
+++ internal/utils/excel/excel.go
@@ -1,26 +1,37 @@
 package excel
 
 import (
+	"componentmod/internal/models"
 	"componentmod/internal/utils/log"
+	"encoding/json"
 	"fmt"
 	"os"
 
 	"github.com/golang-module/carbon"
 	"github.com/pkg/errors"
+	"github.com/tidwall/gjson"
 	"github.com/xuri/excelize/v2"
 
 	"componentmod/internal/utils/file"
 )
 
 var (
-	folderPath    string
 	extensionName = ".xls"
 	sheetName     = "shopee"
-	header        = []string{"產品ID(蝦皮ID)", "產品名稱", "敘述", "其他(勿調整)", "分類", "主圖片", "圖片(逗號區隔)", "蝦皮連結"}
+	// header        = []string{"產品ID(蝦皮ID)", "產品名稱", "敘述", "其他(勿調整)", "分類", "主圖片", "圖片(逗號區隔)", "蝦皮連結"}
+	headerList = []map[string]string{ //依照寫入順序 對照代號
+		1: {"ProductId": "產品ID(蝦皮ID)"},
+		2: {"Name": "產品名稱"},
+		3: {"Description": "敘述"},
+		4: {"Option": "其他選項"},
+		5: {"Categories": "分類"},
+		6: {"Image": "主圖片"},
+		7: {"Images": "圖片(逗號區隔)"},
+		8: {"Url": "蝦皮連結"}}
 )
 
-// func WriteExcel(ShopeeDataModel shopeeData) {
-func WriteExcel(folderPath string) error {
+func WriteExcel(folderPath string, DataModelList []*models.ShopeeDataModel) error {
+	// func WriteExcel(folderPath string) error {
 	dateTime := carbon.Now(carbon.Taipei).Format("Y-m-d")
 	filePath := folderPath + "/" + dateTime + extensionName
 	if file.FileIsExist(folderPath) == false {
@@ -33,44 +44,94 @@ func WriteExcel(folderPath string) error {
 		}
 	}
 
+	//創建檔案、寫入標題
 	if file.FileIsExist(filePath) == false {
-		//創建檔案、寫入標題
 		f := excelize.NewFile()
 		f.SetSheetName("Sheet1", sheetName)
-		err := newFileAndHeader(filePath, f, header)
+
+		err := writeExcelData(filePath, f, func() ([][]string, int, int) {
+			//寫入順序
+			var content [][]string
+			var detail []string
+
+			for _, mapValue := range headerList {
+				for _, v := range mapValue {
+					detail = append(detail, v)
+				}
+			}
+
+			content = append(content, detail)
+			rowCount := 1
+			rowStart := 1
+			return content, rowCount + rowStart, rowStart
+		})
+
 		if err != nil {
 			return err
 		}
 	}
 
-	// f, err := excelize.OpenFile(filePath)
-	// if err != nil {
-	// 	log.Warn(err)
-	// 	return
-	// }
+	//資料excel 寫入
+	f, err := excelize.OpenFile(filePath)
+	err = writeExcelData(filePath, f, func() ([][]string, int, int) {
+		//寫入順序
+		var content [][]string
+		var detail []string
 
-	// shopeeData
+		for _, DataMode := range DataModelList {
+			jsData, err := json.Marshal(DataMode)
+			if err != nil {
+				log.Warn(fmt.Sprintf("轉型錯誤,ProductId:%v , Name:%v", DataMode.ProductId, DataMode.Name))
+				continue
+			}
+			for _, mapValue := range headerList {
+				for _, v := range mapValue {
+					// detail = append(detail, v)
+					detail = append(detail, gjson.Get(string(jsData), v).String())
+				}
+			}
+
+			content = append(content, detail)
+		}
+		rowCount := len(DataModelList)
+		rowStart := 2
+		return content, rowCount + rowStart, rowStart
+	})
+	if err != nil {
+		return err
+	}
+
 	return nil
 }
 
-func newFileAndHeader(filePath string, f *excelize.File, content []string) error {
-	streamWriter, err := f.NewStreamWriter(sheetName)
+func writeExcelData(filePath string, f *excelize.File, fun func() ([][]string, int, int)) error {
+	fmt.Println(f.GetSheetName(0))
+	streamWriter, err := f.NewStreamWriter("shopee")
 	if err != nil {
 		errContent := errors.New(fmt.Sprintf("串流寫入excel錯誤"))
 		errData := errors.WithMessage(err, errContent.Error())
 		return errData
 	}
 
-	rowData := make([]interface{}, len(content))
-	for i, s := range content {
-		rowData[i] = s
-	}
+	defer streamWriter.Flush()
 
-	cell, _ := excelize.CoordinatesToCellName(1, 1)
-	if err := streamWriter.SetRow(cell, rowData); err != nil {
-		errContent := errors.New(fmt.Sprintf("row寫入excel錯誤"))
-		errData := errors.WithMessage(err, errContent.Error())
-		return errData
+	content, rowCount, rowStart := fun()
+	arrcounter := -1
+	for row := rowStart; row < rowCount; row++ {
+		var rowData []interface{}
+		arrcounter++
+		for _, s := range content[arrcounter] {
+			for _, value := range s {
+				rowData = append(rowData, value)
+			}
+		}
+
+		cell, _ := excelize.CoordinatesToCellName(1, row)
+		if err := streamWriter.SetRow(cell, rowData); err != nil {
+			errContent := errors.New(fmt.Sprintf("row寫入excel錯誤"))
+			errData := errors.WithMessage(err, errContent.Error())
+			return errData
+		}
 	}
 
 	if err := f.SaveAs(filePath); err != nil {
--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"componentmod/internal/models"
 	"componentmod/internal/utils/excel"
 	"componentmod/internal/utils/log"
 	"fmt"
@@ -36,8 +37,21 @@ func main() {
 
 	//todo 寫入 exce  utils/excel
 	mydir, _ := os.Getwd()
-	err := excel.WriteExcel(mydir + "/excel")
+	var options []models.Options
+	options = append(options, models.Options{Name: "size", Option: []string{"xs", "s", "m", "l", "xl"}})
+	options = append(options, models.Options{Name: "color", Option: []string{"yello", "red", "black"}})
+	shopeeModel := &models.ShopeeDataModel{}
+	shopeeModel.ProductId = 123
+	shopeeModel.Name = "5165"
+	shopeeModel.Description = "asdf"
+	shopeeModel.Option = options
+	shopeeModel.Categories = "asdf"
+	shopeeModel.Image = "www://asdf"
+	shopeeModel.Images = "www://asdf,www://adfa,www://asdfaksdfj,www://asdfasdf"
+	shopeeModel.Url = "www://asdf"
+	err := excel.WriteExcel(mydir+"/excel", []*models.ShopeeDataModel{shopeeModel})
 	if err != nil {
 		log.Error(fmt.Sprintf("%+v", err))
 	}
+
 }
