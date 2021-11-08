f2963f0b00cf4cd396ca1fc46452cb8f81a560bf
Gary <Gary.shih@wvt.com.tw>
2021-11-08 16:03:07 +0800

    修正 預設回傳值DTO

--- go.mod
+++ go.mod
@@ -7,13 +7,11 @@ require (
 	github.com/cpuguy83/go-md2man/v2 v2.0.1 // indirect
 	github.com/gin-gonic/gin v1.7.4
 	github.com/go-redis/cache/v8 v8.4.3
-	github.com/go-redis/redis v6.15.9+incompatible
 	github.com/go-redis/redis/v8 v8.11.4
 	github.com/golang-module/carbon v1.5.3
-	github.com/onsi/gomega v1.16.0 // indirect
 	github.com/pkg/errors v0.9.1
 	github.com/sirupsen/logrus v1.8.1
-	github.com/tidwall/gjson v1.8.1
+	github.com/tidwall/gjson v1.11.0
 	github.com/urfave/cli/v2 v2.3.0
 	github.com/xuri/excelize/v2 v2.4.1
 	golang.org/x/text v0.3.7 // indirect
--- internal/api/controller/production_controller.go
+++ internal/api/controller/production_controller.go
@@ -18,15 +18,20 @@ var (
 )
 
 func GetProduction(c *gin.Context) (Data, error) {
-	var shopeeProduction *dto.ShopeeProductionInDTO
+	var shopeeProduction = &dto.ShopeeProductionInDTO{
+		Page:           1,
+		PageLimit:      20,
+		Sort:           "asc",
+		SortColumn:     "PId",
+		Search:         "",
+		SearchCategory: "",
+	}
 	err := c.Bind(&shopeeProduction)
-
 	if err != nil {
 		errData := errors.WithMessage(errors.WithStack(err), errorCode.PARAMETER_ERROR)
 		log.Warn(fmt.Sprintf("%+v", errData))
 		return nil, utils.CreateApiErr(errorCode.PARAMETER_ERROR_CODE, errorCode.PARAMETER_ERROR)
 	}
-	fmt.Printf("%+v\n", shopeeProduction)
 
 	shService := api.GetShopeeService()
 	return shService.Production(shopeeProduction)
