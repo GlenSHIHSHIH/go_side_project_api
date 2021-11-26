b0a669fe2abf4707f2e21e00ba7983b64f570995
Gary <Gary.shih@wvt.com.tw>
2021-11-26 12:00:08 +0800

    新增  cache 參數 與註解 未來如需從db 修改請增加cache

--- internal/services/api/forestage/base_forestage_service.go
+++ internal/services/api/forestage/base_forestage_service.go
@@ -3,6 +3,12 @@ package forestage
 import (
 	"componentmod/internal/api/config"
 	"componentmod/internal/dto/forestage"
+	"time"
+)
+
+const (
+	CACHE_FORESTAGE_CONFIG      = "cache_forestage_config"
+	CACHE_FORESTAGE_CONFIG_TIME = 10 * time.Minute
 )
 
 type BaseForestageService struct {
@@ -14,6 +20,8 @@ func GetBaseForestageService() *BaseForestageService {
 
 // 基礎參數
 func (b *BaseForestageService) GetBaseConfig() (interface{}, error) {
+
+	//未來從資料庫讀取 需增加cache
 
 	baseConfigDTO := &forestage.BaseConfigDTO{
 		ImgUrl: config.ImgUrl,
