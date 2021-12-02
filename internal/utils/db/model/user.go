6786281ec188263c7689bb4e56c6059ac20ad7b0
Gary <Gary.shih@wvt.com.tw>
2021-12-02 15:31:18 +0800

    修改  model  ，增加密碼欄位長度 (100)、帳號改為唯一值、登入時間 取消default

--- internal/utils/db/model/user.go
+++ internal/utils/db/model/user.go
@@ -9,14 +9,14 @@ import (
 type User struct {
 	Id            int            `gorm:"primaryKey" json:"id"`
 	Name          string         `gorm:"comment:使用者名稱;type:varchar(30)" json:"name"`                                      //使用者名稱
-	LoginName     string         `gorm:"comment:登入帳號;type:varchar(30)" json:"loginName"`                                  //登入帳號
-	Password      string         `gorm:"comment:密碼;type:varchar(50)" json:"password"`                                     //密碼
+	LoginName     string         `gorm:"comment:登入帳號;type:varchar(30);uniqueIndex" json:"loginName"`                      //登入帳號
+	Password      string         `gorm:"comment:密碼;type:varchar(100)" json:"password"`                                    //密碼
 	Email         string         `gorm:"comment:Email;type:varchar(50)" json:"email"`                                     //Email
 	Status        bool           `gorm:"comment:帳號狀態(false停用 true正常);type:bool;default:true" json:"status"`               //帳號狀態(false停用 true正常)
 	UserType      bool           `gorm:"comment:是否為系統用戶;type:bool;default:true" json:"UserType"`                          //是否為系統用戶
 	LoginIP       string         `gorm:"comment:ip;type:varchar(20)" json:"loginIP"`                                      //loginIP
 	PwdUpdateTime time.Time      `gorm:"comment:密碼最後更新時間;type:datetime;default:CURRENT_TIMESTAMP" json:"pwdUpdateTime"`   //密碼最後更新時間
-	LoginTime     time.Time      `gorm:"comment:最後登入時間;type:datetime;default:CURRENT_TIMESTAMP" json:"loginTime"`         //最後登入時間
+	LoginTime     time.Time      `gorm:"comment:最後登入時間;type:datetime;" json:"loginTime"`                                  //最後登入時間
 	CreateTime    time.Time      `gorm:"comment:新增時間;type:datetime;not null;default:CURRENT_TIMESTAMP" json:"createTime"` //新增時間
 	UpdateTime    time.Time      `gorm:"comment:更新時間;type:datetime;not null;default:CURRENT_TIMESTAMP" json:"updateTime"` //更新時間
 	CreateUserId  int            `gorm:"comment:新增人員Id;type:int" json:"createUserId"`                                     //新增人員
