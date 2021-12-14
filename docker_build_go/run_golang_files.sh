e3beaf65d7740e2d90da0c41dc7ed70f7a0466cb
Gary <Gary.shih@wvt.com.tw>
2021-12-14 09:49:08 +0800

    修改 shell 檔

--- docker_build_go/run_golang_files.sh
+++ docker_build_go/run_golang_files.sh
@@ -22,6 +22,5 @@ export jwt_secret="f9946c78-f48a-435d-acc4-4bf469ef2680"
 export jwt_token_time="480"
 export jwt_ref_token_time="1440"
 
-cd /go/golang-shopee/docker_build_go
-nohup ./compiler/golang_file shopee-data-to-db &
-nohup ./compiler/golang_file shopee-api &
+nohup ./golang_file shopee-data-to-db &
+nohup ./golang_file shopee-api &
